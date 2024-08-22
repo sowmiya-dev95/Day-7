acb44478462846dd5732ae0d353d32ccf3f557af
sowmiya-dev95 <sowmiyarjss95@gmail.com>
2024-08-22 20:54:29 +0530

    Add files via upload

---
+++ Day-7/index.js.js
@@ -0,0 +1,12 @@
+
+//var tweet = prompt("compose your tweeet:"); 
+//alert("you have written" + tweet.length + " characters, you have " + (140-tweet.length ) + " characters remaining.")
+
+
+
+/*you have written 182 characters, you have -42 characters left.*/
+
+var name ="Sowmiya";
+name.slice(0,1);
+name.slice(2,3);
+name.slice(0,3);
---
+++ Day-7/index4.js.js
@@ -0,0 +1,12 @@
+prompt("What is your name?");
+prompt("What is their name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) +1;//1-100
+
+if (loveScore > 70){
+     alert("Your love score is " + loveScore + "%" + "You love each other like kanye loves kanye");
+} else {
+    alert("Your love score is " + loveScore + "%");
+}
+
